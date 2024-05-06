830a9a76838203200763f50c4eb4cce9332f01cb
Emerson452 <emerson@popoyoko.fr>
2024-05-06 14:58:56 +0200

    🍱 add storybook in default app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
-import Popoyoko from './assets/Popoyoko.png'
-import './App.css'
+
+import Popoyoko from "./assets/Popoyoko.png";
+import Storybook from "./assets/storybook.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div>
         <a href="https://github.com/Popoyoko" target="_blank">
           <img src={Popoyoko} className="logo" alt="Popoyoko logo" />
         </a>
-      </div>
-      <h1>Popoyoko Boilerplate</h1>
-      <p className="read-the-docs">
-        Click on the Popoyoko logo to learn more
-      </p>
+          <h1>Popoyoko Boilerplate</h1>
+      <a href="https://popoyoko.github.io/popoyoko-ui/" target="_blank">
+          <img src={Storybook} className="features" alt="Storybook logo" />
+        </a>
+      <p className="read-the-docs">Click on the Popoyoko logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
